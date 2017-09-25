cd71b3443e01c1e7a3eb5801c76f061c76353151
Luis Leal Debian8 <wichofer89@gmail.com>
2017-09-25 02:09:40 -0600

    Finished developing and testing serve a saved R model

--- RModels/model2/run_model.R
+++ RModels/model2/run_model.R
@@ -1,18 +1,14 @@
-
+model_path <- paste0(Sys.getenv("R_MODEL2_DIRECTORY"),"/","model.rda")
 model_params <- commandArgs(trailingOnly = TRUE)
 
-# Predict using given params
 # Load pre-saved model
-load("model.rda")
-
-# Read parameters and convert to single row dataframe
-input_dataframe <-     data.frame(cyl = as.numeric(model_params))
-
+load(model_path)
+# Use parameters to create a single row dataframe
+input_dataframe  <-     data.frame(cyl = as.numeric(model_params))
 
 # Predict on parameter data using restored model
 prediction <- predict(model,input_dataframe)
 
-
 # return result
-#cat(prediction)
-cat(0)
+cat(prediction)
+
